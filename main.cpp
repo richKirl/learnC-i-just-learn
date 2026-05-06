d3f2c203a69759d89128d389be7500a301ac67ad
richKirl <162905102+richKirl@users.noreply.github.com>
2026-05-07 02:52:30 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 import Task1;
-//5
-template <typename... Args>
-auto first_arg(Args... args) {
-    return args...[0];
-}
-int main(){
-    //1
+//1
+void teststatic(){
     auto p = hello_static("Hello Static");
     println("{}",p);
-
-    //2
+}
+//2
+void testhello(){
     print_it("Hello World!");
-
-    //3
+}
+//3
+void testph(){
     mutex my_mutex;
     auto _ = lock_guard(my_mutex);
-
-    //4
+}
+//4
+void testdph(){
     tuple<char,int,char> geek;
 
     // Assigning values to tuple using make_tuple()
     geek = std::make_tuple('a', 10, 15.5);
 
     auto& [a, _, _] = geek;
+}
+//5
+template <typename... Args>
+auto first_arg(Args... args) {
+    return args...[0];
+}
+int main(){
+
+
     return 0;
 }
