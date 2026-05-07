a4c54ce28069397fed511a58da21adac31b2d2f0
richKirl <162905102+richKirl@users.noreply.github.com>
2026-05-07 21:41:35 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void testdph(){
     tuple<char,int,char> geek;
 
     // Assigning values to tuple using make_tuple()
-    geek = std::make_tuple('a', 10, 15.5);
+    geek = std::make_tuple('a', 10, 'r');
 
     auto& [a, _, _] = geek;
 }
@@ -32,12 +32,54 @@ template <typename... Args>
 auto last_arg(Args... args) {
     return args...[sizeof...(args)-1];
 }
+
+//7
+void test7(){
+    test();
+}
+//8
+void test8(){
+    Point<int> x{10,11};
+    x.print();
+    Point<float> x1{10.000001f,11.0000001f};
+    x1.print();
+    Point<double> x2{10.000001,11.0000001};
+    x2.print();
+}
+//9
+void test9(){
+    Point<int> x{10,11};
+    println("{}",x.get_value());
+    Point<float> x1{10.000001f,11.0000001f};
+    println("{}",x1.get_value());
+    Point<double> x2{10.000001,11.0000001};
+    println("{}",x2.get_value());
+
+
+}
 int main(){
+    //teststatic();
+    //testhello();
+    //testph();
+    //testdph();
+    //auto temp = first_arg(1,2,3,4,5,6);
+    //print_it(to_string(temp));
 
-    auto temp = first_arg(1,2,3,4,5,6);
-    print_it(to_string(temp));
+    //temp = last_arg(1,2,3,4,5,6);
+    //print_it(to_string(temp));
 
-    temp = last_arg(1,2,3,4,5,6);
-    print_it(to_string(temp));
+    //test7();
+
+    //test8();
+    //test9();
+    vector<int> p;
+    p.reserve(100);
+    //p.resize(100);
+    int count =0;
+    for (int i=0;i<100;i++) {
+        p.push_back(i);
+        //println("{} {}",p.capacity(),p.size());
+    }
+    println("{}",p);
     return 0;
 }
